352c6179193b4ffde3bbef8e83e128bb921759c4
Stephan Seifermann <seifermann@fzi.de>
2017-10-16 15:03:49 +0200

    [526077] Added missing @since tag
    Signed-off-by: Stephan Seifermann <seifermann@fzi.de>

--- plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/cdo/eresource/impl/IgnoreDetachmentsIfNoNotificationAdapter.java
+++ plugins/org.eclipse.emf.cdo/src/org/eclipse/emf/cdo/eresource/impl/IgnoreDetachmentsIfNoNotificationAdapter.java
@@ -14,6 +14,7 @@ import org.eclipse.emf.common.notify.impl.AdapterImpl;
 
 /**
  * @author Stephan Seifermann
+ * @since 4.6.1
  */
 public class IgnoreDetachmentsIfNoNotificationAdapter extends AdapterImpl
 {
